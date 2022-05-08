4728723c015c0973da36d838ff0fec6e38243b79
RalfMog <104436602+RalfMog@users.noreply.github.com>
2022-05-08 23:26:19 +0300

    git commit;

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,5 +3,21 @@ package jm.task.core.jdbc;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+//        Util.getConnection();
+//        UserDao userDao = new UserDaoJDBCImpl();
+//
+//        userDao.createUsersTable();
+//
+//        userDao.saveUser("Name1", "LastName1", (byte) 20);
+//        userDao.saveUser("Name2", "LastName2", (byte) 25);
+//        userDao.saveUser("Name3", "LastName3", (byte) 31);
+//        userDao.saveUser("Name4", "LastName4", (byte) 38);
+
+//        userDao.removeUserById(1);
+//        userDao.removeUserById(7);
+//        System.out.println(userDao.getAllUsers());
+//        userDao.cleanUsersTable();
+
+//        userDao.dropUsersTable();
     }
 }
